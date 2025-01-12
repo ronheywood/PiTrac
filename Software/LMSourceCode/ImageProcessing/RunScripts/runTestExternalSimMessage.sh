d40b08a76370678ec11a5fef0a85a9dad949008a
James Pilgrim <jpilgrim103@gmail.com>
2025-01-11 20:12:47 -0700

    Added missing common cmd-line parameters.

--- Software/LMSourceCode/ImageProcessing/RunScripts/runTestExternalSimMessage.sh
+++ Software/LMSourceCode/ImageProcessing/RunScripts/runTestExternalSimMessage.sh
@@ -8,4 +8,4 @@
 
 #rm -f Logs/*.log
 
-$PITRAC_ROOT/ImageProcessing/build/pitrac_lm  --system_mode test_sim_message  --search_center_x 700 --search_center_y 650 --logging_level=trace --artifact_save_level=final_results_only
+$PITRAC_ROOT/ImageProcessing/build/pitrac_lm  --system_mode test_sim_message  $PITRAC_COMMON_CMD_LINE_ARGS   --search_center_x 700 --search_center_y 650 --logging_level=info --artifact_save_level=final_results_only

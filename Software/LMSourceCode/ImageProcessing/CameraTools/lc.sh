ed7faa98bb1dceff9d24bde5e3fae16bd5781cdf
James Pilgrim <jpilgrim103@gmail.com>
2025-01-17 14:46:25 -0700

    Sprucing up a few shell scripts.

--- Software/LMSourceCode/ImageProcessing/CameraTools/lc.sh
+++ Software/LMSourceCode/ImageProcessing/CameraTools/lc.sh
@@ -1,2 +1,3 @@
-libcamera-still --nopreview --tuning-file=/usr/share/libcamera/ipa/rpi/vc4/imx477_noir.json --shutter 400 --width 3280 -o $1
+libcamera-still --nopreview --no-raw --tuning-file=/usr/share/libcamera/ipa/rpi/vc4/imx477_noir.json --shutter 400 --width 3280 -o $1
+
 # libcamera-still --nopreview --shutter 200 --tuning-file=/usr/share/libcamera/ipa/rpi/vc4/imx219.json --width 3280 -o $1
--- Software/LMSourceCode/ImageProcessing/CameraTools/lcGS.sh
+++ Software/LMSourceCode/ImageProcessing/CameraTools/lcGS.sh
@@ -1,4 +1,8 @@
+libcamera-still --gain 3.0 --no-raw --nopreview --tuning-file=/usr/share/libcamera/ipa/rpi/pisp/imx296.json --shutter 20000 --width 1456 --height 1088 --denoise cdn_off -o $1
+# Or, for the Pi 4
+#libcamera-still --gain 3.0 --no-raw --nopreview --tuning-file=/usr/share/libcamera/ipa/rpi/vc4/imx296.json --shutter 2000 --width 1456 --height 1088 --denoise cdn_off -o $1
+
+# other examples
 #libcamera-still --nopreview --tuning-file=/usr/share/libcamera/ipa/rpi/vc4/imx296.json --shutter 8000  --denoise cdn_off -o $1
 #libcamera-still --gain 3.0 --nopreview --tuning-file=/usr/share/libcamera/ipa/rpi/vc4/imx296.json --shutter 2000 --width 96 --height 88 --denoise cdn_off -o $1
-libcamera-still --gain 3.0 --nopreview --tuning-file=/usr/share/libcamera/ipa/rpi/vc4/imx296.json --shutter 2000 --width 1456 --height 1088 --denoise cdn_off -o $1
 #libcamera-still --nopreview --tuning-file=/usr/share/libcamera/ipa/rpi/vc4/imx296_noir.json --shutter 2000 --width 1440 --denoise cdn_off -o $1

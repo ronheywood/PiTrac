af4e681b8ccb23456666b4a571a7bec6c9e30708
James Pilgrim <jpilgrim103@gmail.com>
2025-01-19 14:07:03 -0700

    Automating setting up tomee artifacts.

---
+++ Software/LMSourceCode/ImageProcessing/golfsim_tomee_webapp/refresh_from_dev.sh
@@ -0,0 +1,18 @@
+# After running this script, then do a "mvn package" to compile and then  
+# /opt/tomee/bin/restart.sh  
+mkdir -p src/main/webapp/WEB-INF
+mkdir -p src/main/java/com/verdanttechs/jakarta/ee9
+cp $PITRAC_ROOT/ImageProcessing/golfsim_tomee_webapp/src/main/java/com/verdanttechs/jakarta/ee9/MonitorServlet.java ./src/main/java/com/verdanttechs/jakarta/ee9/  
+cp $PITRAC_ROOT/ImageProcessing/golfsim_tomee_webapp/src/main/webapp/WEB-INF/*.jsp ./src/main/webapp/WEB-INF  
+cp $PITRAC_ROOT/ImageProcessing/golfsim_tomee_webapp/src/main/webapp/*.html ./src/main/webapp  
+cp $PITRAC_ROOT/ImageProcessing/golfsim_tomee_webapp/pom.xml .  
+# Also pull over the current .json configuration file to make sure that the webapp is looking at the correct version.  
+cp $PITRAC_ROOT/ImageProcessing/golf_sim_config.json ~/LM_Shares/WebShare/  
+
+#Point the URL for the PiTrac Gui to the right configuration file location
+echo $PITRAC_WEBSERVER_SHARE_DIR > webserver_name.tmp.txt
+sed -i 's/\//%2F/g' webserver_name.tmp.txt
+
+sed -i 's@PITRAC_WEBSERVER_SHARE_DIR@'`cat webserver_name.tmp.txt`'@g' ./src/main/webapp/index.html
+
+rm webserver_name.tmp.txt

f59f25e164ccdedfb9659a71a10118adad347e2f
James Pilgrim <jpilgrim103@gmail.com>
2025-05-15 22:28:58 -0600

    Fixed runTestResults.sh problem where the common arguments were missing.

--- Software/LMSourceCode/ImageProcessing/RunScripts/runTestResults.sh
+++ Software/LMSourceCode/ImageProcessing/RunScripts/runTestResults.sh
@@ -8,6 +8,6 @@
 . $PITRAC_ROOT/ImageProcessing/RunScripts/runPiTracCommon.sh
 
 
-$PITRAC_ROOT/ImageProcessing/build/pitrac_lm --send_test_results  --system_mode camera1_test_standalone  --search_center_x 700 --search_center_y 600 --logging_level=trace
+$PITRAC_ROOT/ImageProcessing/build/pitrac_lm --send_test_results  $PITRAC_COMMON_CMD_LINE_ARGS --system_mode camera1_test_standalone  --search_center_x 700 --search_center_y 600 --logging_level=trace
 
 # --post-process-file /mnt/VerdantShare/dev/GolfSim/LM/ImageProcessing/assets/motion_detect.json

7aaebadbde930436dfa29cfd9f315f4b689ac0ad
Ron Heywood <ron.heywood@spektrix.com>
2025-05-21 19:53:40 +0100

    chocolatey install dependencies

--- dev-setup/windows/psakefile.ps1
+++ dev-setup/windows/psakefile.ps1
@@ -8,3 +8,14 @@ Task Help {
     Write-Host "Example usage:" -ForegroundColor Green
     Write-Host "  Invoke-psake .\run.psake.ps1 Help"
 }
+
+Task Install {
+    Write-Host "Installing PiTrac dependencies..." -ForegroundColor Green
+    # Install Chocolatey if not already installed
+    if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+        Write-Host "Installing Chocolatey..." -ForegroundColor Yellow
+        Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+
+    choco install .\choco-packages.config
+}

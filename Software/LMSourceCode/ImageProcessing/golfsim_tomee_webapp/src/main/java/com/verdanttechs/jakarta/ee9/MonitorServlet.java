7adf85da83445eea3696730f8a4b3245d4c1d332
James Pilgrim <121443783+jamespilgrim@users.noreply.github.com>
2025-06-12 10:56:47 -0600

    Update MonitorServlet.java
    If the ActiveMQ address cannot be found as an environment variable, it will now be picked up from the golf_sim_config.json file.

--- Software/LMSourceCode/ImageProcessing/golfsim_tomee_webapp/src/main/java/com/verdanttechs/jakarta/ee9/MonitorServlet.java
+++ Software/LMSourceCode/ImageProcessing/golfsim_tomee_webapp/src/main/java/com/verdanttechs/jakarta/ee9/MonitorServlet.java
@@ -579,7 +579,7 @@ public class MonitorServlet extends HttpServlet {
     private static String kGolfSimTopic = "Golf.Sim";
     // Set from environment variable or default. This can be overidden from config file later
     private static String kWebActiveMQHostAddress = System.getenv("PITRAC_MSG_BROKER_FULL_ADDRESS") != null ?
-            System.getenv("PITRAC_MSG_BROKER_FULL_ADDRESS") : "tcp://10.0.0.41:61616";
+            System.getenv("PITRAC_MSG_BROKER_FULL_ADDRESS") : "";
     private static String kWebServerTomcatShareDirectory;
     private static String kWebServerResultBallExposureCandidates;
     private static String kWebServerResultSpinBall1Image;
